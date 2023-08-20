f55339ddb4e21de7ee26bf94d85cdf9d6b803661
AlexKomanov <akomanov88@gmail.com>
2023-08-20 18:38:50 +0300

    Assignment class task

---
+++ src/part_03_assignment_operators/classTasks/AssignmentTask.java
@@ -0,0 +1,21 @@
+package part_03_assignment_operators.classTasks;
+
+public class AssignmentTask {
+    public static void main(String[] args) {
+
+        int a = 10;
+
+//        a += 90;
+//        a -= 25;
+//        a /= 3;
+//        a %= 10;
+//        a *= (-3);
+//        a++;
+//        a--;
+
+        System.out.println("a = " + a);
+
+        System.out.println("a = " + (((((((a+90)-25)/3)%10)*-3)+1)-1));
+    }
+    }
+
