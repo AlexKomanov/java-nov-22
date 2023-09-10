7437863a54b1546dffff52cc4bf654a7a74930f2
AlexKomanov <akomanov88@gmail.com>
2023-09-10 18:29:23 +0300

    Adding Java Final task 1

---
+++ src/part_07_final_exercises/FinalQuestion1.java
@@ -0,0 +1,15 @@
+package part_07_final_exercises;
+
+import java.util.Scanner;
+
+public class FinalQuestion1 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello, What is your age? ");
+        int age = input.nextInt();
+
+        System.out.println("Well, now I know you are " + age + " years old!");
+
+    }
+}
