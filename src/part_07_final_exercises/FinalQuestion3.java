afec9ca57aa3509a7ebf07e6b2fc6825bd07c2c4
AlexKomanov <akomanov88@gmail.com>
2023-09-10 18:29:34 +0300

    Adding Java Final task 3

---
+++ src/part_07_final_exercises/FinalQuestion3.java
@@ -0,0 +1,18 @@
+package part_07_final_exercises;
+
+import java.util.Scanner;
+
+public class FinalQuestion3 {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a word: ");
+        String word = scan.nextLine();
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            System.out.print(word.charAt(i));
+
+        }
+
+    }
+}
